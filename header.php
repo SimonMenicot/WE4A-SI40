85656f4b8e44406f4f6e3b2b0ee64e380cc524f3
Simon <simon.menicot@heig-vd.ch>
2025-03-11 18:45:04 +0100

    Add logo + modif font

--- header.php
+++ header.php
@@ -8,12 +8,18 @@ function drawHeader(string $title): void
 	<!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo $title ?> - Nooble</title>
-	<link rel="stylesheet" href="<?php echo NOOBLE_CONFIG["SERVER"]["PATH_NAME"] ?>/static/styles/index.css"/>
-	<script src="<?php echo NOOBLE_CONFIG["SERVER"]["PATH_NAME"] ?>/static/scripts/index.js"></script>
+	<title><?= $title ?> - Nooble</title>
+	<link rel="stylesheet" href="<?= NOOBLE_CONFIG["SERVER"]["PATH_NAME"] ?>/static/styles/index.css"/>
+	<link rel="icon" type="image/png" href="<?= NOOBLE_CONFIG["SERVER"]["PATH_NAME"] ?>/static/images/logo.png">
+	<script src="<?= NOOBLE_CONFIG["SERVER"]["PATH_NAME"] ?>/static/scripts/index.js"></script>
 </head>
 <body>
-	<header>Nooble</header>
+	<header>
+		<div id="head">
+			<img src="<?= NOOBLE_CONFIG["SERVER"]["PATH_NAME"] ?>/static/images/logo.png" width="48" height="48">
+			<h1><a href="<?= NOOBLE_CONFIG["SERVER"]["PATH_NAME"] ?>">Nooble</a></h1>
+		</div>
+	</header>
 	<div id="root">
 		<main>
 	<?php
