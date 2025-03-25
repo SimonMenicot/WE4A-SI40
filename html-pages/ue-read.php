5a096aa13c4c5e229fe294cce4995cedb11091a8
Kaki In <kaki@mifamofi.net>
2025-03-25 15:33:46 +0100

    Some changes  - added the course-page  - eated some colombo. That was delicious.

---
+++ html-pages/ue-read.php
@@ -0,0 +1,187 @@
+<?php
+set_include_path("../");
+require "header.php";
+drawHeader("Welcome, Student");
+?>
+<div id="classpage-main-separation" class="content-separator">
+    <div id="main-class-content" class="class-content">
+        <div class="class-title-content">
+            <a class="class-back-link" href=".">
+                <img src="<?= NOOBLE_CONFIG["SERVER"]["PATH_NAME"] ?>/static/images/icons/back.png">
+            </a>
+            <h1>
+                WE4A - Introduction au design WEB
+            </h1>
+        </div>
+
+        <div id="main-class-section" class="section section-container">
+            <div class="section section-container">
+                <p class="section section-raw-text">
+                    Ceci est un texte brute dans un container vertical
+                </p>
+
+                <p class="section section-raw-text">
+    Qu'est-ce que le Lorem Ipsum ?<br>
+    Le Lorem Ipsum est simplement un faux texte utilisé dans l'industrie de l'impression et du traitement de texte. Depuis les années 1500, il est considéré comme le texte fictif standard, lorsque quelqu'un a pris une galère de caractères pour la mélanger et créer un livre d'exemple typographique. Il est parvenu à survivre non seulement cinq siècles, mais aussi l'entrée dans le traitement électronique du texte, sans être essentiellement modifié. Sa popularité a été renforcée dans les années 1960 avec la sortie des feuilles Letraset contenant des passages de Lorem Ipsum, et plus récemment grâce aux logiciels de publication assistée par ordinateur comme Aldus PageMaker qui incluent des versions de Lorem Ipsum.<br>
+    Le morceau standard de Lorem Ipsum utilisé depuis les années 1500 est reproduit ci-dessous pour ceux que cela intéresse. Les sections 1.10.32 et 1.10.33 de "De Finibus Bonorum et Malorum" par Cicéron y sont également reproduites dans leur forme originale exacte, accompagnées des versions anglaises traduites en 1914 par H. Rackham.
+                </p>
+
+                <div class="section section-rich-text">
+                    <h2>
+                        <em>Ceci est un texte riche</em>
+                    </h2>
+                    <p>
+                        <strong>Charles Stein</strong> est un statisticien américain né le 22 mars 1920 et mort le 24 novembre 2016, professeur émérite de statistiques à <a href="https://fr.wikipedia.org/wiki/Universit%C3%A9_Stanford" target="_blank"> Stanford</a>.
+                        Il est connu pour le <a href="https://fr.wikipedia.org/wiki/Paradoxe_de_Stein" target="_blank">paradoxe de Stein</a> en <a href="https://fr.wikipedia.org/wiki/Th%C3%A9orie_de_la_d%C3%A9cision" target="_blank">théorie de la décision</a> et pour la <a href="https://fr.wikipedia.org/wiki/M%C3%A9thode_de_Stein" target="_blank">méthode de Stein</a> qui permet de démontrer le <a href="https://fr.wikipedia.org/wiki/Th%C3%A9or%C3%A8me_central_limite" target="_blank"> central limite</a>.
+                    </p>
+                    <p>
+                        <strong>Auguste Van Assche</strong>, né le 5 juillet 1826 à <a href="https://fr.wikipedia.org/wiki/Gand" target="_blank">Gand</a> et mort le 24 février 1907, est un <a href="https://fr.wikipedia.org/wiki/Architecte" target="_blank">architecte</a> <a href="https://fr.wikipedia.org/wiki/Belgique" target="_blank">belge</a>, auteur de nombreuses restaurations et transformations de bâtiments médiévaux en Belgique.
+                    </p>
+                </div>
+            </div>
+
+            <p class="section section-raw-text">
+                Ce qui suit est un container horizontal avec wrap (miam miam)
+            </p>
+
+            <div class="section section-container horizontal-section-container wrapping-section-container">
+                <div class="section section-file">
+                    <p class="file-description">TD1 : Sûrement pas sur Symfony mdr, débrouillez vous</p>
+                    <a class="file-button" href="https://tests-and-previews.flopcreation.fr/downloads/UTBM - Nooble/TD1.zip" target="_blank">
+                        <img src="<?= NOOBLE_CONFIG["SERVER"]["PATH_NAME"] ?>/static/images/icons/download.png">
+                        <text class="file-extension">TD1.zip</text>
+                    </a>
+                </div>
+
+                <div class="section section-file">
+                    <p class="file-description">TD2 : Tiens, et si on faisait du PHP?</p>
+                    <a class="file-button" href="https://tests-and-previews.flopcreation.fr/downloads/UTBM - Nooble/TD2.zip" target="_blank">
+                        <img src="<?= NOOBLE_CONFIG["SERVER"]["PATH_NAME"] ?>/static/images/icons/download.png">
+                        <text class="file-extension">TD2.zip</text>
+                    </a>
+                </div>
+
+                <div class="section section-file">
+                    <p class="file-description">TD3 : Symfony? Noooon, AJAX bien sûr!</p>
+                    <a class="file-button" href="https://tests-and-previews.flopcreation.fr/downloads/UTBM - Nooble/TD3.zip" target="_blank">
+                        <img src="<?= NOOBLE_CONFIG["SERVER"]["PATH_NAME"] ?>/static/images/icons/download.png">
+                        <text class="file-extension">TD3.zip</text>
+                    </a>
+                </div>
+
+                <div class="section section-file">
+                    <p class="file-description">Le cahier des charges : ce que vous allez devoir faire en 2 semaines, en même temps que les autres projets, que vos clubs, que votre vie en général en fait. Ou pas. </p>
+                    <a class="file-button" href="https://tests-and-previews.flopcreation.fr/downloads/UTBM - Nooble/WE4A - Moodle Simplifié (devoir retiré).pdf" target="_blank">
+                        <img src="<?= NOOBLE_CONFIG["SERVER"]["PATH_NAME"] ?>/static/images/icons/download.png">
+                        <text class="file-extension">WE4A - Moodle Simplifié (devoir retiré).pdf</text>
+                    </a>
+                </div>
+            </div>
+
+            <div class="section section-container horizontal-section-container">
+                <img class="section section-image" src="https://randomwordgenerator.com/img/picture-generator/55e2dc434c50a814f1dc8460962e33791c3ad6e04e50744074267bd19f4ac2_640.jpg"/>
+                <img class="section section-image" src="https://randomwordgenerator.com/img/picture-generator/5fe4d0474255b10ff3d8992cc12c30771037dbf85254784b772779d69f4e_640.jpg"/>
+            </div>
+
+            <div id="an_id_that_will_be_replaced_by_js" class="section section-activity">
+                <!--- The activities will be integrated with Javascript, and may not need to user "document.getElementById" --->
+                <p>
+                    Ceci est une activité interactive. 
+                </p>
+
+                <button id="activity-button">
+                    !Cliquez-moi!
+                </button>
+                
+                <br>
+
+                <small>
+                The activities will be integrated with Javascript, and may not need to user "document.getElementById
+                </small>
+
+                <script>
+                    let defined_outside_there_div = document.getElementById("an_id_that_will_be_replaced_by_js");
+                    defined_outside_there_div.querySelector("button").addEventListener("click", () => {
+                        alert("Vous avez cliqué!!!");
+                    })
+                </script>
+            </div>
+
+            <div class="section section-container horizontal-section-container">
+                <iframe class="section section-integration" width="560" height="315" src="https://www.youtube.com/embed/lJIrF4YjHfQ?si=bNKfRgR0zomAqao8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p class="section section-raw-text">
+    Qu'est-ce que le Lorem Ipsum ?<br>
+    Le Lorem Ipsum est simplement un faux texte utilisé dans l'industrie de l'impression et du traitement de texte. Depuis les années 1500, il est considéré comme le texte fictif standard, lorsque quelqu'un a pris une galère de caractères pour la mélanger et créer un livre d'exemple typographique. Il est parvenu à survivre non seulement cinq siècles, mais aussi l'entrée dans le traitement électronique du texte, sans être essentiellement modifié. Sa popularité a été renforcée dans les années 1960 avec la sortie des feuilles Letraset contenant des passages de Lorem Ipsum, et plus récemment grâce aux logiciels de publication assistée par ordinateur comme Aldus PageMaker qui incluent des versions de Lorem Ipsum.<br>
+    Le morceau standard de Lorem Ipsum utilisé depuis les années 1500 est reproduit ci-dessous pour ceux que cela intéresse. Les sections 1.10.32 et 1.10.33 de "De Finibus Bonorum et Malorum" par Cicéron y sont également reproduites dans leur forme originale exacte, accompagnées des versions anglaises traduites en 1914 par H. Rackham.
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+    <section id="activity-thread" class="side-thread">
+        <div class="activity">
+            <h1>
+                Activité recent
+            </h1>
+            <div class="activity-content">
+                <p>
+                    J'ai créé un Figma
+                </p>
+
+                <div class="activity-author">
+                    <img style="background:linear-gradient(to bottom right, red, blue)"/>
+                    <p>Nicolas Daval</p>
+                </div>
+
+                <time>
+                    Le 24/03/2025 à 10:00
+                </time>
+            </div>
+        </div>
+
+        <div class="activity">
+            <h1>
+                Devoir à rendre
+            </h1>
+            <div class="activity-content">
+                <p>
+                    J'ai créé un Figma
+                </p>
+
+                <div class="activity-author">
+                    <img style="background:linear-gradient(to bottom right, red, blue)"/>
+                    <p>Nicolas Daval</p>
+                </div>
+
+                <time>
+                    Le 24/03/2025 à 10:00
+                </time>
+            </div>
+        </div>
+
+        <div class="activity">
+            <h1>
+                Salut tout le monde!
+            </h1>
+            <div class="activity-content">
+                <p>
+                    J'aime beaucoup les sardines au beurre, soit dit en passant ; pourquoi se borner à manger des haricots?
+                </p>
+
+                <div class="activity-author">
+                    <img style="background:linear-gradient(to right, red, green)"/>
+                    <p>Eden Morey</p>
+                </div>
+
+                <time>
+                    24/03/2025 19:25
+                </time>
+            </div>
+        </div>
+    </section>
+</div>
+
+<?php
+	require "footer.php";
+?>
