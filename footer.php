a751dd9b52b7f472215ae5b903fae0c77d42263d
Kaki In <kaki@mifamofi.net>
2025-04-01 18:19:50 +0200

    Enhanced feature in footer Added last commit information

--- footer.php
+++ footer.php
@@ -1,5 +1,23 @@
-		</main>
-		<footer>Thanks for using Nooble</footer>
+<?php
+
+if (NOOBLE_CONFIG === null) {
+	include "config.php";
+};
+
+//git log --pretty=\"%ci\" -n1 HEAD
+?>		</main>
+		<footer>
+			Thanks for using Nooble<br>
+			<?php 
+$content = file_get_contents($_SERVER["DOCUMENT_ROOT"].NOOBLE_CONFIG["SERVER"]["PATH_NAME"]."/.git/FETCH_HEAD");
+
+if ($content === false) return;
+
+$lines = preg_split("/\n/", $content);
+
+echo $lines[0];
+?>
+		</footer>
 	</div>
 </body>
 </html>
