3de6a5ea458a015765a4fe51e24be22092897cfd
Nikoala4 <daval.nicolas4@gmail.com>
2025-04-06 21:42:28 +0200

    added link to project hosted just in case config doesn't work properly

--- config.php
+++ config.php
@@ -4,6 +4,9 @@
 
     This is the configuration file. 
 
+    The WIP project is hosted on this server just in case you need to test it :
+    https://tests-and-previews.flopcreation.fr/projects/nooble/WE40-SI40/html-pages/generated-dynamic-ue-edit.php
+
     Configurable elements : 
      - SERVER : 
          - PATH_NAME : the root path FROM THE SERVER to access the project. 
